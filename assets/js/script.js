18269a0907d82b84ec684b8005a6acfeebd8e93a
meri-sargsyan1 <meri.sargsyan.08@mail.ru>
2024-12-10 22:31:51 +0400

    car

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,47 @@
-const root = document.querySelector('.root');
+// const root = document.querySelector('.root');
 
 
-const items = [
-    {
-        name:'Легковые ',
-        imgSrc:'./assets/img/Rectangle.svg'
-    },
-    {
-        name:'Легковые ',
-        imgSrc:'./assets/img/Rectangle.svg'
-    },
-    {
-        name:'Легковые ',
-        imgSrc:'./assets/img/Rectangle.svg'
-    },
-    {
-        name:'Легковые ',
-        imgSrc:'./assets/img/Rectangle.svg'
-    },
-];
-const div = document.createElement('div');
-const div1 = document.createElement('div')
-root.appendChild(div);
-root.appendChild(div1);
-items.forEach((item,index)=>{
+// const items = [
+//     {
+//         name:'Легковые ',
+//         imgSrc:'./assets/img/lexkavie.svg'
+//     },
+//     {
+//         name:'Легковые ',
+//         imgSrc:'./assets/img/fur.svg'
+//     },
+//     {
+//         name:'Легковые ',
+//         imgSrc:'./assets/img/motociklet.svg'
+//     },
+//     {
+//         name:'Легковые ',
+//         imgSrc:'./assets/img/maslo.svg'
+//     },
+//     {
+//         name:'Легковые ',
+//         imgSrc:'./assets/img/acsesuari.svg'
+//     },
+// ];
+// const div = document.createElement('div');
+// const div1 = document.createElement('div')
+// root.appendChild(div);
+// root.appendChild(div1);
+// items.forEach((item,index)=>{
     
-    const itemDiv = document.createElement('p');
-    itemDiv.textContent = item.name;
-    const imgElement = document.createElement('img');
-    imgElement.src = item.imgSrc;
+//     const itemDiv = document.createElement('p');
+//     itemDiv.textContent = item.name;
+//     const imgElement = document.createElement('img');
+//     imgElement.src = item.imgSrc;
 
-   div.appendChild(itemDiv);
-   div1.appendChild(imgElement);
+//    div.appendChild(itemDiv);
+//    div1.appendChild(imgElement);
     
     
-})
+// })
+
+
+function createElement(){
+
+}
 
